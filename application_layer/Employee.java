012f36e202b90b61fa8cb088cc2cd9aea4f57413
= <t8230154@aueb.gr>
2025-11-20 15:07:48 +0200

    updating employee class

--- application_layer/Employee.java
+++ application_layer/Employee.java
@@ -1,29 +1,27 @@
 package application_layer;
 
-import java.util.*;
 
 public class Employee extends User { 
+    private int managerId;
     private int daysOffRemaining ;
-    private List<JobCalendar> calendar ;
 
     public Employee() {} 
-
     public Employee(int id, String name, String surname, String username, String password, String email, int companyId,int daysOffRemaining,
-     List<JobCalendar> calendar) {
+     int managerId) {
         super(id,name,surname,username,password,email,companyId);
+        this.managerId = managerId;
         this.daysOffRemaining = daysOffRemaining;
-        this.calendar = calendar;
     }
 
    
     public int getDaysOffRemaining() {return this.daysOffRemaining;}
     public void setDaysOffRemaining(int daysOffRemaining) {this.daysOffRemaining = daysOffRemaining;}
-    public List<JobCalendar> getCalendar() {return this.calendar;}
-    public void setCalendar(List<JobCalendar> calendar) {this.calendar = calendar;}
+    public int getMnagerId() {return this.managerId;}
+    public void setManagerId(int managerId) {this.managerId = managerId;}
 
     @Override
     public String toString() {
-        return super.toString() + ", Days Off Remaining:" + daysOffRemaining ;
+        return super.toString() + "Manager Id :" + managerId + ", Days Off Remaining:" + daysOffRemaining ;
     }
     
 }
