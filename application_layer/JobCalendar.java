5d6154302ef913acf1a03778d963cd2f1e59b8f2
ArisOIKON <t8230109@aueb.gr>
2025-11-20 22:30:58 +0200

    Added some methods

--- application_layer/JobCalendar.java
+++ application_layer/JobCalendar.java
@@ -1,21 +1,33 @@
 package application_layer;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
+
 
 public class JobCalendar {
+
+
     private int calendarId;
     private int managerId;
     private LocalDate dateCreated;
     private LocalDate startingDate;
     private LocalDate endingDate;
+     
+    private Map<String, List<Shift>> shiftsByDay = new HashMap<>();
+    
+    DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
 
     public JobCalendar() {}
-    public JobCalendar(int calendarId, int managerId, LocalDate dateCreated, LocalDate startingDate, LocalDate endingDate) {
+    public JobCalendar(int calendarId, int managerId, LocalDate dateCreated, LocalDate startingDate, LocalDate endingDate, List<Employee> employees) {
         this.calendarId = calendarId;
         this.managerId = managerId;
         this.dateCreated = dateCreated;
         this.startingDate = startingDate;
         this.endingDate = endingDate;
+        
     }
 
     public int getCalendarId() {return this.calendarId;}
@@ -28,6 +40,68 @@ public class JobCalendar {
     public void setStartingTime(LocalDate startingDate) {this.startingDate = startingDate;}
     public LocalDate getEndingDate() {return this.endingDate;}
     public void setEndingDate(LocalDate endingDate) {this.endingDate = endingDate;}
+    
+
+
+    public void FillJobCalendar(List<Shift> shifts) {
+
+        shiftsByDay = shifts.stream()
+        .collect(Collectors.groupingBy(Shift::getDay));
+
+
+        for (String day : List.of(
+            "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY",
+            "FRIDAY", "SATURDAY", "SUNDAY")) {
+            shiftsByDay.putIfAbsent(day, new ArrayList<>());
+        }
+        
+   
+
+    }
+    //The first method returns the weekly shifts of the employees. The second one returns the shifts of a specific day
+    
+    public Map<String, List<Shift>> getJobCalendar() {
+        return shiftsByDay;
+    } 
+     
+    public List<Shift> getShiftsForDay(String day) {
+        return shiftsByDay.getOrDefault(day, Collections.emptyList());
+    }
+
+    public Map<String, List<Shift>> getAllShifts() {
+        return shiftsByDay;
+    }
+    
+
+    public void removeShift(Shift oldShift) {
+        
+        //we might need to check if the times and date is null here.(1)
+        if (oldShift == null) return;
+
+        String dayKey = oldShift.getDay().toUpperCase();
+        
+        // Get the list for the dayKey. This object (dayShifts) is the exact same list that is mapped to the shift's day
+        List<Shift> dayShifts = shiftsByDay.get(dayKey);
+        
+        if (dayShifts.contains(oldShift)) {
+            // Remove the shift from that day's list. The HashMap is also updated here
+            dayShifts.remove(oldShift);
+        }
+    }
+    
+    public void addShift(Shift newShift) {
+        //(1)
+        if (newShift == null) return;
+        String dayKey = newShift.getDay().toUpperCase();
+        List<Shift> dayShifts = shiftsByDay.get(dayKey);
+
+        //The shift is added in any case unless it in null
+        dayShifts.add(newShift);
+
+
+
+    }
+
 
     @Override 
     public String toString() {
--- application_layer/Shift.java
+++ application_layer/Shift.java
@@ -1,57 +1,94 @@
 package application_layer;
 
 import java.time.*;
-import java.time.format.*;
+import java.time.format.DateTimeFormatter;
 
+
+
+//This implementation allows for the employee to have multiple shifts in a day without any problems. I suggest we avoid creating double shifts for now 
+//and later, if we decide that double shifts ought to be disallowd we can discuss possible solutions later. Nevertheless the flow of the programm is not affected by this.
 public class Shift {
     private int shiftId;
     private int employeeId;
     private int managerId;
     private int calendarId;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
-    private LocalDate date; 
-    
-    
+    private LocalDate date;             
+    private LocalTime startTime; 
+    private LocalTime endTime;
+
     public Shift() {}
-    public Shift(int shiftId, int employeeId, int managerId, int calendarId, LocalDateTime startTime, LocalDateTime endTime, LocalDate date) {
-        this.shiftId = shiftId;
+
+    public Shift(int shiftId, int employeeId, int managerId, int calendarId, LocalDate date, LocalTime startTime, LocalTime endTime) {
+
+
+        // I'm not sure if the validity of the date inputs will be checked here
+        if (!endTime.isAfter(startTime)) {
+            throw new IllegalArgumentException("End time must be after start time.");
+        }
+
         this.employeeId = employeeId;
         this.managerId = managerId;
-        this.calendarId = calendarId;
+        this.shiftId = shiftId;
+        this.date = date;
         this.startTime = startTime;
         this.endTime = endTime;
-        this.date = date;
     }
-    public int getShiftId() {return this.shiftId;}
-    public void setShiftId(int shiftId) {this.shiftId = shiftId;}
-    public int getEmployeeId() {return this.employeeId;}
-    public void setEmployeeId(int employeeId) {this.employeeId = employeeId;}
-    public int getManagerId() {return this.managerId;}
-    public void setManagerId(int managerId) {this.managerId = managerId;}
-    public int getCalendarId() {return this.calendarId;}
+
+ 
+    public int getEmployeeID() {return employeeId;}
+    public int getShiftID() {return shiftId;}
+    public int getManagerID() {return managerId;}
+    public int getCalendarId() {return calendarId;}
+    public LocalDate getDate() {return date;}
+    public LocalTime getStartTime() {return startTime;}
+    public LocalTime getEndTime() {return endTime;}
+    public void setEmployeeID(Integer employeeId) {this.employeeId = employeeId;}
+    public void setShiftID(Integer shiftId) {this.shiftId = shiftId;}
+    public void setManagerID(Integer managerId) {this.managerId = managerId;}
     public void setCalendarId(int calendarId) {this.calendarId = calendarId;}
-    public LocalDateTime getStartTime() {return this.startTime;}
-    public void setStartTime(LocalDateTime startTime) {this.startTime = startTime;}
-    public LocalDateTime getEndTime() {return this.endTime;}
-    public void setEndTime(LocalDateTime endTime) {this.endTime = endTime;}
-    public LocalDate getDate() {return this.date;}
     public void setDate(LocalDate date) {this.date = date;}
+    public void setStartTime(LocalTime startTime) {this.startTime = startTime;}
+    public void setEndTime(LocalTime endTime) {this.endTime = endTime;}
+    
+
+    public String getDay() {
+        return this.date.getDayOfWeek().name();
+
+    }
+
+
+    public Duration getDuration() {
+        return Duration.between(startTime, endTime);
+    }
+    
+
+    //This could be implemented with the set methods, I thought it makes more sense to get all the info that needs to change as a single method
+    public void editShift(LocalDate newDate, LocalTime newStart, LocalTime newEnd){
+
+        this.date = newDate;
+        this.startTime = newStart;
+        this.endTime = newStart;
+
+    }
+
+
 
 
     @Override
     public String toString() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
-           return String.format(
-           "Shift %s, employeeId %s, managerId %s, caledarId %s, (%s → %s),  date %s",
-           shiftId,
-           employeeId,
-           managerId,
-           calendarId,
-           startTime.format(formatter),
-           endTime.format(formatter),
-           date
-         
-       );
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+        
+
+        return String.format(
+            "Shift %s, employeeId %s, managerId %s, caledarId %s, (%s → %s),  date %s",
+            shiftId,
+            employeeId,
+            managerId,
+            calendarId,
+            startTime.format(timeFormatter),
+            endTime.format(timeFormatter), 
+            date
+           
+        );
     }
-}
+}  
