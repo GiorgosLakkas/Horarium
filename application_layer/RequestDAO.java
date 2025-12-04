46dfd4d13c438a1b01c75f813fabbb9d3c8fe9f8
= <t8230154@aueb.gr>
2025-12-04 16:50:58 +0200

    updating methods for retrieving ShiftChange or Absence requests

--- application_layer/RequestDAO.java
+++ application_layer/RequestDAO.java
@@ -79,11 +79,11 @@ public class RequestDAO {
         }
     }
 
-    //method for absence request retrieval 
+     //method for absence request retrieval 
     public List<AbsenceRequest> retrieveManagerAbsenceRequests(int managerId) {
         List<AbsenceRequest> requests = new ArrayList<>();
         Connection con = DBConnection.openConnection();
-        String sql = "SELECT * FROM Request as r, AbsenceRequest as a, Manager as m WHERE r.manager_id = m.id AND r.request_id = a.request_id AND manager_id = ?";
+        String sql = "SELECT a.request_id,employee_id,manager_id,date_created,status,start_date,end_date,absence_type FROM Request as r, AbsenceRequest as a, Manager as m WHERE r.manager_id = m.id AND r.request_id = a.request_id AND manager_id = ?";
         try {
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setInt(1,managerId);
@@ -113,7 +113,7 @@ public class RequestDAO {
     public List<ShiftChangeRequest> retrieveManagerShiftChangeRequests(int managerId) {
         List<ShiftChangeRequest> requests = new ArrayList<>();
         Connection con = DBConnection.openConnection();
-        String sql = "SELECT * FROM Request as r, ShiftChangeRequest as s, Manager as m WHERE r.manager_id = m.id AND r.request_id = s.request_id AND manager_id = ?";
+        String sql = "SELECT s.request_id,employee_id,manager_id,date_created,status,old_shift_date,new_shift_date, starting_time, ending_time FROM Request as r, ShiftChangeRequest as s, Manager as m WHERE r.manager_id = m.id AND r.request_id = s.request_id AND manager_id = ?";
         try {
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setInt(1,managerId);
@@ -121,7 +121,7 @@ public class RequestDAO {
             while (rs.next()) {
                 int requestId = rs.getInt("request_id");
                 int employeeId = rs.getInt("employee_id");
-                LocalDate date = rs.getDate("date").toLocalDate();
+                LocalDate date = rs.getDate("date_created").toLocalDate();
                 String stringStatus = rs.getString("status");
                 Request.Status status = Request.Status.valueOf(stringStatus.toUpperCase());
                 LocalDate oldShiftDate = rs.getDate("old_shift_date").toLocalDate();
