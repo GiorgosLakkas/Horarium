c2b3ec966e8b3259f4c047ea0c66835786689f8d
= <t8230154@aueb.gr>
2025-12-19 21:52:54 +0200

    adding first DTO class for crucial absence request data transfering

---
+++ application_layer/AbsenceDTO.java
@@ -0,0 +1,24 @@
+package application_layer;
+
+import java.time.LocalDate;
+
+//DTO stands for Data Transfering Object
+public class AbsenceDTO {
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private int daysOff;
+
+    public AbsenceDTO() {}
+    public AbsenceDTO(LocalDate startDate, LocalDate endDate, int daysOff) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.daysOff = daysOff;
+    }
+    
+    public LocalDate getStartDate() {return startDate;}
+    public LocalDate getEndDate() { return endDate;}
+    public int getDaysOff() {return this.daysOff;}
+    public void setStartDate(LocalDate startDate) {this.startDate = startDate;}
+    public void setEndDate(LocalDate endDate) {this.endDate = endDate;}
+    public void setDaysOff(int daysOff) {this.daysOff=daysOff;}
+}
