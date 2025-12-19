b458a3625b9a4587e4c0b5397d295e40999a4058
= <t8230154@aueb.gr>
2025-12-19 22:00:52 +0200

    adding new DAO methods for shifts

--- application_layer/ShiftDAO.java
+++ application_layer/ShiftDAO.java
@@ -100,9 +100,78 @@ public class ShiftDAO {
         } finally {
             DBConnection.closeConnection(con);
         }
-        
-        
     }
-
-
+        //method for fetching all crucial data about a shift to be changed, based on an accepted shift change request
+    public Shift fetchShiftDataFromRequestByEmployeeId(LocalDate date) throws Exception {
+        Connection con = DBConnection.openConnection();
+        String sql = "SELECT * FROM Shift as s,Request as r WHERE s.employee_id = r.employee_id AND s.date = ?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setDate(1,java.sql.Date.valueOf(date));
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return new Shift(rs.getInt("s.shift_id"),
+                rs.getInt("s.employee_id"),
+                rs.getInt("s.manager_id"),
+                rs.getInt("s.job_calendar_id"),
+                rs.getTime("s.start_time").toLocalTime(),
+                rs.getTime("s.end_time").toLocalTime(), date);
+            } else {
+                throw new Exception("No shift at this date exists");
+            }
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally {
+            DBConnection.closeConnection(con);
+        }
+    }
+    //method for putting a shift in a the Shift table
+    public void insertShift(Shift shift) throws Exception { 
+        Connection con = DBConnection.openConnection();
+        String sql = "INSERT INTO Shift(employee_id,manager_id,job_calendar_id,start_time,end_time,date) VALUES(?,?,?,?,?,?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, shift.getEmployeeId());
+            ps.setInt(2,shift.getManagerId());
+            ps.setInt(3,shift.getCalendarId());
+            ps.setTime(4, java.sql.Time.valueOf(shift.getStartTime()));
+            ps.setTime(5, java.sql.Time.valueOf(shift.getEndTime()));
+            ps.setDate(6, java.sql.Date.valueOf(shift.getDate()));
+            int row = ps.executeUpdate();
+            if (row == 0) {
+                throw new Exception("No shift was inserted");
+            }
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally {
+            DBConnection.closeConnection(con);
+        }
+    }
+    //method for retrieving remaining shifts of an employee from current date until the job calendar's end date
+    public List<Shift> fetchRemainingWeeklyShiftsByEmployeeId(int employeeId) throws Exception {
+        List<Shift> shifts = new ArrayList<>();
+        Connection con = DBConnection.openConnection();
+        String sql = "SELECT * FROM Shift AS s WHERE employee_id = ? AND date >= CURDATE() AND date <= (SELECT ending_date FROM Job_Calendar AS j WHERE j.calendar_id = s.job_calendar_id)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1,employeeId);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                shifts.add(new Shift(
+                    rs.getInt("shift_id"),
+                    employeeId,
+                    rs.getInt("manager_id"),
+                    rs.getInt("job_calendar_id"),
+                    rs.getTime("start_time").toLocalTime(),
+                    rs.getTime("end_time").toLocalTime(),
+                    rs.getDate("date").toLocalDate()
+                ));
+            }
+            return shifts;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally {
+            DBConnection.closeConnection(con);
+        }
+    }
 }
